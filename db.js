2287305020d023b81c5baa069fb05b719ae20993
aditya <aaditya1392@gmail.com>
2023-04-10 08:56:45 +0530

    cors

--- db.js
+++ db.js
@@ -1,6 +1,13 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
+app.use(
+    cors({
+      origin: "*",
+      credentials: true
+    })
+  )
 app.use(express.json())
 
 
